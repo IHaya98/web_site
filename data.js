88e6277cd3b78094a86fc62869fc88ec1594ab64
izakura <hayatte-izakura@kccs.co.jp>
2020-06-04 17:55:53 +0900

    ganttView new

---
+++ data.js
@@ -0,0 +1,8 @@
+var ganttData = [
+	{
+		id: 1, name: "Feature 1", series: [
+			{ name: "Planned", start: new Date(2020,00,01,08,10), end: new Date(2020,00,03,18,10) },
+			{ name: "Actual", start: new Date(2020,00,02,08,10), end: new Date(2020,00,05,18,10), color: "#f0f0f0" }
+		]
+	}
+];
